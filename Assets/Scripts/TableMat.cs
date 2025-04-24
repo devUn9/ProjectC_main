4e8d611a87287e42022111c4a029b6f21d776bb1
Jino-Lee <90809030+Leetany@users.noreply.github.com>
2025-04-24 18:01:11 +0900

    홀로그램 적용 완료

---
+++ Assets/Scripts/TableMat.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class TableMat : MonoBehaviour
+{
+    private SpriteRenderer _spriteRenderes;
+    private Material _materials;
+
+    [SerializeField] private float _hologramSpeed = 0.05f;
+    private float hologram_position;
+
+    private int _verticalDissolveAmount = Shader.PropertyToID("_VerticalDissolve");
+
+    void Start()
+    {
+        _spriteRenderes = GetComponent<SpriteRenderer>();
+        _materials = _spriteRenderes.material;
+
+        hologram_position = 0f;
+    }
+
+    
+    void Update()
+    {
+        hologram_position += _hologramSpeed * Time.deltaTime;
+
+        _materials.SetFloat(_verticalDissolveAmount, hologram_position);
+
+        if(hologram_position > 0.43f)
+        {
+            hologram_position = 0;
+        }
+    }
+}
