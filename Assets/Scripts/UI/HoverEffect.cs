002cc731f40cf5f57d71f3af709ae07f8ccd45fa
Jino-Lee <90809030+Leetany@users.noreply.github.com>
2025-04-28 08:43:02 +0900

    옵션 창 시각 효과 추가

---
+++ Assets/Scripts/UI/HoverEffect.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Vector3 hoverOffset; // 마우스 오버 시 이동할 위치
+    private Vector3 originalPosition;
+
+    private void Start()
+    {
+        // 원래 위치 저장
+        originalPosition = transform.localPosition;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // 마우스가 버튼 위로 갔을 때
+        transform.localPosition = originalPosition + hoverOffset;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // 마우스가 버튼에서 나갔을 때
+        transform.localPosition = originalPosition;
+    }
+}
