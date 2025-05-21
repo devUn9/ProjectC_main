1656281449152978bc631065b765a33f8d3d5899
Jino-Lee <90809030+Leetany@users.noreply.github.com>
2025-05-21 22:00:41 +0900

    보스 hp 적용

--- Assets/Scripts/Stats/Boss1Stats.cs
+++ Assets/Scripts/Stats/Boss1Stats.cs
@@ -1,19 +1,28 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Boss1Stats : CharacterStats
 {
     private Boss1 boss1 => GetComponent<Boss1>();
 
+    private float hp;
+    private float hp_Cur;
+
+    [SerializeField] Image hpBar_Front;
+    [SerializeField] Image hpBar_Back;
+
+
     protected override void Start()
     {
         base.Start();
-
+        hp_Cur = (float)currentHealth;
+        hp = (float)maxHealth.GetValue();
     }
 
     protected override void Update()
     {
         base.Update();
-
+        SyncBar();
 
     }
     public override void TakeDamage(int _damage)
@@ -41,5 +50,14 @@ public class Boss1Stats : CharacterStats
         return false;
     }
 
+    private void SyncBar()
+    {
+        hp_Cur = (float)currentHealth;
+        hpBar_Front.fillAmount = hp_Cur / hp;
 
+        if (hpBar_Back.fillAmount > hpBar_Front.fillAmount)
+        {
+            hpBar_Back.fillAmount = Mathf.Lerp(hpBar_Back.fillAmount, hpBar_Front.fillAmount, Time.deltaTime);
+        }
+    }
 }
